b116116fc227c7f71baf586d80378b8b59df8a48
Rohit Paul <rohitpaul9734@gmail.com>
2025-04-03 00:20:35 +0530

    Next step

---
+++ src/component/Header/LogOutBtn.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import authService from "../../appWrite/auth";
+import { logout } from "../../Store/authSlice";
+
+
+
+
+function LogOutBtn() {
+  const dispatch = useDispatch();
+  const logoutHandeler = () => {
+    authService.logout().then(() => {
+      dispatch(logout());
+    });
+  };
+  return (
+    <button className="inline-block px-6 py-2 duration-200 hover:bg-blue-200 rounded-full">
+      Logout
+    </button>
+  );
+}
+
+export default LogOutBtn;
---
+++ src/component/index.js
@@ -0,0 +1,17 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogOutBtn from "./Header/LogOutBtn";
+import Input from "./Input";
+import Button from "./Button"
+
+export { 
+    Header, 
+    Footer, 
+    Container, 
+    Logo, 
+    LogOutBtn,
+    Input,
+    Button
+};
