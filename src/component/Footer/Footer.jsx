e3213e6c37577a9fd0743bd319c986dd1b6bbda8
Rohit Paul <rohitpaul9734@gmail.com>
2025-07-09 20:33:22 +0530

    .

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import Logo from "../Logo";
 function Footer() {
   return (
-    <section className="relative overflow-hidden py-4 bg-slate-800">
+    <section className="relative bottom-0 overflow-hidden py-4 bg-slate-800">
       <div className="relative z-10 mx-auto max-w-7xl px-4 flex items-center justify-between">
         <div className="mb-4 inline-flex text-white items-center">
           <Logo width="100px" />
